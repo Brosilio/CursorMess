6ab47eecbe00fd01de0c9a90b4b7ad9d6dce3674
Brosilio <brosilio@youshouldfear.me>
2020-07-26 20:27:28 -0400

    is now functional now goes by "room" so users only see other users on the same page is now more efficient

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,21 +1,24 @@
-import fs from 'fs';
-
 import express from 'express';
 import expressws from 'express-ws';
 import WebSocket from 'ws';
-import msgpack from 'msgpack-lite';
 
-interface MiiiClient {
+const MAX_CURSORS_PER_CLIENT = 16;
+const CLIENT_IDLE_TIMEOUT_SECONDS = 64;
+
+interface Asshole {
     id: number,
     ws: WebSocket,
-	ping: any
+    ping: any,
+    cursorCount: number,
+    lastUpdate: number,
+    urlHash: string
 }
 
 console.debug('cursormess');
 
 var stats = {
-	bytes_snd: 0,
-	bytes_rcv: 0,
+    bytes_snd: 0,
+    bytes_rcv: 0,
     most_assholes: 0,
     broadcast: 0,
     sent: 0,
@@ -25,13 +28,13 @@ var stats = {
 const app = expressws(express()).app;
 
 var accu = 0;
-const clients: MiiiClient[] = [];
+const rooms = {};
 
-function broadcast(data, exclude) {
-
+function broadcast(room, data, exclude) {
     stats.broadcast++;
 
-    clients.forEach(x => {
+    rooms[room].forEach(x => {
+        // skip over the excluded client
         if (exclude != null && exclude != undefined) {
             if (Array.isArray(exclude)) {
                 if (exclude.includes(x)) {
@@ -44,10 +47,20 @@ function broadcast(data, exclude) {
             }
         }
 
+        // don't bother sending data to idle clients, they might not even be looking at their screen
+        if (data.c == 'p' && ((Date.now() - x.lastUpdate) / 1000) > CLIENT_IDLE_TIMEOUT_SECONDS) {
+            return;
+        }
+
+        // decrement their cursor count so new ones can be sent to them later
+        if (data.c == 'r') {
+            x.cursorCount--;
+        }
+
         try {
             if ((x.ws.readyState === WebSocket.OPEN)) {
                 stats.sent++;
-                x.ws.send(data);
+                x.ws.send(JSON.stringify(data));
             }
         }
         catch (e) { }
@@ -55,12 +68,36 @@ function broadcast(data, exclude) {
 
 }
 
+function broadacastNewCursor(room, client) {
+    let msg = {
+        c: 'a',
+        id: client.id
+    };
+
+    broadcast(room, msg, client);
+}
+
+function addToRoom(client) {
+    const room = client.urlHash;
+    if (rooms[room] == undefined) {
+        rooms[room] = [];
+    }
+    
+    rooms[room].push(client);
+    broadacastNewCursor(room, client);
+
+    console.log("someone joined room" + room);
+}
+
 app.ws("/cursormess", (ws, req) => {
 
-    const client: MiiiClient = {
+    const client: Asshole = {
         id: accu++,
         ws,
-        ping: null
+        ping: null,
+        cursorCount: 0,
+        lastUpdate: Date.now(),
+        urlHash: null
     };
 
     function send(data) {
@@ -73,21 +110,60 @@ app.ws("/cursormess", (ws, req) => {
         catch (e) { }
     }
 
-    clients.push(client);
     console.log(`${client.id}: open from ${req.ip}`);
 
+    // tell the new client about all the other clients
+
     ws.on('close', () => {
-        clients.splice(clients.indexOf(client), 1);
+        rooms[client.urlHash].splice(rooms[client.urlHash].indexOf(client), 1);
+        if(rooms[client.urlHash].length == 0) {
+            console.log("nobody left in room " + client.urlHash);
+            rooms[client.urlHash] = undefined;
+            return; // dont need to send updates to the room cause its gone
+        }
         console.log(`${client.id}: closed`);
+        let msg = {
+            c: 'r',
+            id: client.id
+        }
+        broadcast(client.urlHash, msg, client);
     });
 
-    ws.on('message', msg => {
-
+    ws.on('message', raw => {
         stats.recv++;
-        console.log(msg);
+
         try {
-			// broadcast incoming data
-            broadcast(msg, client);
+            client.lastUpdate = Date.now();
+            // broadcast incoming data
+            // TODO: currently, this will send *any* incoming data to all the clients. rework to only send the valid params to avoid exploits
+            const data = JSON.parse(raw as string);
+            let msg = {
+                ...data,
+                id: client.id
+            };
+            // is join message, contains url of client.
+            if (msg.c == 'j') {
+                client.urlHash = msg.urlHash;
+                addToRoom(client);
+                for (let i = 0; i < rooms[client.urlHash].length; i++) {
+                    const c = rooms[client.urlHash][i];
+                    if (c.id == client.id)
+                        continue;
+
+                    let msg = {
+                        c: 'a',
+                        id: c.id
+                    };
+
+                    send(JSON.stringify(msg));
+                    client.cursorCount++;
+
+                    if (client.cursorCount >= MAX_CURSORS_PER_CLIENT)
+                        break;
+                }
+            } else {
+                broadcast(client.urlHash, msg, client);
+            }
         }
         catch (e) {
             console.error(`${client.id}: exception while processing message`);
